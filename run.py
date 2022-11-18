970d6461a652e611893bc7a49343f77cd6c1fa65
OmriHadad747 <omrihadad747@gmail.com>
2022-11-18 22:36:49 +0200

    1

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from skip_db_lib import config
 
 if __name__ == "__main__":
     app = create_app(config.DevConfig)
-    app.run(host="localhost", port=4997, debug=True)
+    app.run(host="localhost", port=4998, debug=True)
