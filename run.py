ae81c01bdb6c0e87a02d726b5981576f20fbb024
OmriHadad747 <omrihadad747@gmail.com>
2022-11-19 22:29:39 +0200

    1

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import config
 
 if __name__ == "__main__":
     app = create_app(config.DevConfig)
-    app.run(host="localhost", port=4998, debug=True)
+    app.run(host="0.0.0.0", port=4998, debug=True)
