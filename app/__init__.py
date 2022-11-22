037df53156562ec2a87450efa15818adf295272f
OmriHadad747 <omrihadad747@gmail.com>
2022-11-22 14:06:11 +0200

    moving to skip-common

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,22 @@
-from app import config
 from flask import Flask
 
+from skip_common_lib.config import BaseConfig
 
-def create_app(app_config: config.BaseConfig) -> Flask:
+
+def create_app(app_config: BaseConfig) -> Flask:
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config)
 
     with app.app_context():
-        # init flask-extensions
-        from app.extensions import firebase_admin, firebase_admin_creds
+        from skip_common_lib.database import mongo
+        mongo.init_app(app)
 
+        import firebase_admin
+        from skip_common_lib.extensions import jwt, firebase_admin_creds
         firebase_admin.initialize_app(firebase_admin_creds)
+        jwt.init_app(app)
 
-        from skip_db_lib import database
-
-        database.mongo.init_app(app)
-
-        from app import routes
-
-        app.register_blueprint(routes.job_quotation_bp)
+        from app.routes import quotation
+        app.register_blueprint(quotation.job_quotation_bp)
 
         return app
--- app/extensions.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-
-from firebase_admin import credentials
-from flask import current_app as app
-
-
-firebase_admin_creds = credentials.Certificate(app.config["FIREBASE_SERVICE_ACCOUNT"])
